c131abd954f0d7a2708c988239d03b6939924a84
Kenny <mccormick84@gmail.com>
2022-02-03 19:01:03 +0900

    타입스크립트로 컴포넌트 작성하기(4)
    - useReducer 사용하기

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Profile from './Profile';
 import {SafeAreaView, Text} from 'react-native';
 import MessageForm from './MessageForm';
+import Counter from './Counter';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Text>Hello World</Text>
       </Profile>
       <MessageForm />
+      <Counter />
     </SafeAreaView>
   );
 }
